6edc262799c836c32e023bad123269539827de75
Frederik Müller <frederik-christian.mueller@stud.uni-bamberg.de>
2016-05-24 14:16:57 +0200

    add package and class for Runnable used in REDUCEMAP strategy

---
+++ src/main/java/reducemap/MappingRunnable.java
@@ -0,0 +1,13 @@
+package reducemap;
+
+public class MappingRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		// calculate distance to cluster centroids and assign point to new
+		// cluster
+
+	}
+
+}
